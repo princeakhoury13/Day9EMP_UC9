c58ba1950df00cdd1e7072297b22ff74be2891f6
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-20 12:28:03 +0530

    added: UC 9 solution

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Day9EMPWageUC9
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmpWageBuilder infosys = new EmpWageBuilder("Infosys", 22, 3, 18);
+            EmpWageBuilder tcs = new EmpWageBuilder("TATA CONSULTANCY SERVICES", 15, 4, 25);
+
+            infosys.computeEmpWage();
+            Console.WriteLine(infosys.toString());
+            tcs.computeEmpWage();
+            Console.WriteLine(tcs.toString());
+        }
+    }
+}
